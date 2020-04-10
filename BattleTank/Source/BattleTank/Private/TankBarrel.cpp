ef8b068a42cb89f03c61296db7f884a5ccdfe7c7
DeTress <fmk-remi@hotmail.fr>
2020-04-10 17:09:15 +0200

    Clamping

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,9 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Engine/World.h"
 #include "BattleTank/BattleTank.h"
 #include "BattleTank/Public/TankBarrel.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
-
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
