259ff11a5bd76f9c56b8f945825e4741890f1ced
DeTress <fmk-remi@hotmail.fr>
2020-04-15 00:27:08 +0200

    Track and base for movement

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is used to set max driving force and to aply forces to the tank
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+
+	// Max Force per Track
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDringForce = 400000; // Assume 40 tonnes, and 1g accélération
+	
+};
