b8cb466252b036e10a42a9d53e720542e87676f0
DeTress <fmk-remi@hotmail.fr>
2020-04-16 00:36:07 +0200

    ai mouvement

--- BattleTank/Source/BattleTank/Private/TankMouvementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMouvementComponent.cpp
@@ -18,9 +18,14 @@ void UTankMouvementComponent::initialise(UTankTrack* LeftTrackToSet, UTankTrack*
 void UTankMouvementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
 	//No need to call super as we're replacing the functionality
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"),*TankName, *MoveVelocityString)
+
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	//UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"),*TankName, *MoveVelocityString)
+	IntendMoveForward(ForwardThrow);
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);
 }
 
 void UTankMouvementComponent::IntendMoveForward(float Throw)
